23d0b35c4557dde5ed2a2a9caa4b7f769b614491
albud187 <alfa.budiman@outlook.com>
2024-12-25 11:36:41 -0500

    works

--- src/gimbal_standalone/launch/velcon.launch.py
+++ src/gimbal_standalone/launch/velcon.launch.py
@@ -15,18 +15,13 @@ for arg in sys.argv:
     if arg.startswith("ax2:="):
         ax2 = float(arg.split(":=")[1])
 
-    if arg.startswith("ax3:="):
-        ax3 = float(arg.split(":=")[1])
-
 ax1 = ax1 * (math.pi/180)
 ax2 = ax2 * (math.pi/180)
-ax3 = ax3 * (math.pi/180)
-
 def generate_launch_description():
     return LaunchDescription([
         launch_ros.actions.Node(
-            package='arm_robot',
-            executable='vel_control.py',
+            package='gimbal_standalone',
+            executable='velcon.py',
             output='screen',
-            arguments=[str(ax1), str(ax2), str(ax3)]),
+            arguments=[str(ax1), str(ax2)]),
     ])
