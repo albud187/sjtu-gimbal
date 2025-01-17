5900147efbb3e3e4482ee8471b8cdc6261b97e79
albud187 <alfa.budiman@outlook.com>
2025-01-17 18:11:22 -0500

    debug

--- _run.sh
+++ _run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #ros2 launch gimbal_standalone launch_sim.launch.py
-#ros2 launch sjtu_drone_bringup sjtu_gimbal_1x_empty_world.launch.py
+ros2 launch sjtu_drone_bringup sjtu_gimbal_1x_empty_world.launch.py
 
-ros2 launch sjtu_drone_bringup sjtu_gimbal_controller.launch.py
+#ros2 launch sjtu_drone_bringup sjtu_gimbal_controller.launch.py
 
